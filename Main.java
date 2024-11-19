95be8f21fbc8ed368aeaa58ad9a907e71a8af8e2
johnnyz27 <johnnyz27@nycstudents.net>
2024-11-18 20:46:42 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,1 +1,15 @@
+import java.util.Arrays;
 
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {4, 3, 2, 8, 3};
+        System.out.println("The integer -2 occurs " + PracticeProblems.countOccurrences(arr, -2) + " times");
+        int[] reversed = PracticeProblems.reverseArray(arr);
+        System.out.println("The reversed elements are " + Arrays.toString(reversed));
+
+        double[][] grid = {{1.1, 2.2}, {3.3, 4.4}};
+        System.out.println("Sum of grid: " + PracticeProblems.sumGrid(grid));
+        
+        System.out.println("Fibonacci number at position 5: " + PracticeProblems.fib(5));
+    }
+}
